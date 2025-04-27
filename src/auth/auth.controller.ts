4c5c2a40e2225a2bae99896bca9816e26b49c3e1
liemak363 <liemak363work@gmail.com>
2025-04-27 19:49:16 +0700

    feat(swagger): add api doc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,39 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiBody({ type: AuthDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Đăng nhập thành công',
+    schema: {
+      example: {
+        access_token: 'jwt.token.here',
+      },
+    },
+  })
   @Post('login')
   login(@Body() dto: AuthDto) {
     // Implement login logic here
     return this.authService.login(dto);
   }
 
+  @ApiBody({ type: AuthDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Đăng ký thành công',
+    schema: {
+      example: {
+        access_token: 'jwt.token.here',
+      },
+    },
+  })
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     // Implement signup logic here
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -12,13 +12,42 @@ import { Request } from 'express';
 import { ProductService } from './product.service';
 import { ProductDto } from './dto';
 import { JwtGuard } from 'src/auth/guard';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiBody,
+  ApiResponse,
+  ApiParam,
+} from '@nestjs/swagger';
 
+@ApiTags('product')
 @UseGuards(JwtGuard)
+@ApiBearerAuth('access-token')
 @Controller('product')
 export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Post('import')
+  @ApiBody({ type: ProductDto })
+  @ApiResponse({
+    status: 201,
+    description:
+      'Tạo mới sản phẩm nếu không tìm thấy theo id hoặc cập nhật sản phẩm thành công',
+    schema: {
+      example: {
+        message: 'Product imported successfully',
+        product: {
+          id: 1,
+          createdAt: '2025-04-27T12:00:00.000Z',
+          updatedAt: '2025-04-27T12:00:00.000Z',
+          name: 'Táo đỏ Mỹ',
+          price: 100000,
+          quantity: 10,
+          description: 'Táo đỏ nhập khẩu từ Mỹ',
+        },
+      },
+    },
+  })
   import(@Body() dto: ProductDto, @Req() req: Request) {
     console.log('Request Headers:', req.user);
     if (!req.user || typeof req.user !== 'number') {
@@ -28,6 +57,22 @@ export class ProductController {
   }
 
   @Get('id/:id')
+  @ApiParam({ name: 'id', type: Number, description: 'ID sản phẩm' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lấy thông tin sản phẩm theo ID',
+    schema: {
+      example: {
+        id: 1,
+        createdAt: '2025-04-27T12:00:00.000Z',
+        updatedAt: '2025-04-27T12:00:00.000Z',
+        name: 'Táo đỏ Mỹ',
+        price: 100000,
+        quantity: 10,
+        description: 'Táo đỏ nhập khẩu từ Mỹ',
+      },
+    },
+  })
   getProductById(@Param('id', ParseIntPipe) id: number, @Req() req: Request) {
     if (!req.user || typeof req.user !== 'number') {
       throw new Error('Invalid user or user ID');
@@ -36,6 +81,22 @@ export class ProductController {
   }
 
   @Get('name/:name')
+  @ApiParam({ name: 'name', type: String, description: 'Tên sản phẩm' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lấy thông tin sản phẩm theo tên',
+    schema: {
+      example: {
+        id: 1,
+        createdAt: '2025-04-27T12:00:00.000Z',
+        updatedAt: '2025-04-27T12:00:00.000Z',
+        name: 'Táo đỏ Mỹ',
+        price: 100000,
+        quantity: 10,
+        description: 'Táo đỏ nhập khẩu từ Mỹ',
+      },
+    },
+  })
   getProductByName(@Param('name') name: string, @Req() req: Request) {
     if (!req.user || typeof req.user !== 'number') {
       throw new Error('Invalid user or user ID');
