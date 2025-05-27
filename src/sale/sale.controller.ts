c38580d55c7790516b60b2314ee538dd4a6e8bf7
liemak363 <liemak363work@gmail.com>
2025-05-28 00:46:51 +0700

    feat(sale): /sale/items/:id to get items of a sale log

--- src/sale/sale.controller.ts
+++ src/sale/sale.controller.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   Controller,
   Get,
+  ParseIntPipe,
   Query,
   Req,
   UseGuards,
@@ -25,5 +26,16 @@ export class SaleController {
       throw new BadRequestException('Invalid user or user ID');
     }
     return this.saleService.getSaleLog(req.user, query);
+  }
+
+  @Get('log/:id')
+  getSaleLogItems(
+    @Query('id', ParseIntPipe) saleLogId: number,
+    @Req() req: Request,
+  ) {
+    if (!req.user || typeof req.user !== 'number') {
+      throw new BadRequestException('Invalid user or user ID');
+    }
+    return this.saleService.getSaleLogItems(req.user, saleLogId);
   }
 }
--- src/sale/sale.service.ts
+++ src/sale/sale.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SaleLogQueryDto } from './dto';
 
@@ -33,6 +33,50 @@ export class SaleService {
 
     return {
       data: saleLogs,
+    };
+  }
+
+  async getSaleLogItems(userId: number, saleLogId: number) {
+    // Kiểm tra xem SaleLog có tồn tại không và thuộc về user hiện tại
+    const saleLog = await this.prisma.saleLog.findUnique({
+      where: {
+        id: saleLogId,
+        userId: userId,
+      },
+    });
+
+    if (!saleLog) {
+      throw new NotFoundException('Sale log not found');
+    }
+
+    // Truy vấn các item của SaleLog kèm theo thông tin sản phẩm
+    const saleLogItems = await this.prisma.saleLogItem.findMany({
+      where: {
+        saleLogId: saleLogId,
+      },
+      include: {
+        product: {
+          select: {
+            name: true,
+            description: true,
+          },
+        },
+      },
+    });
+
+    return {
+      saleLog: saleLog,
+      items: saleLogItems.map((item) => ({
+        saleLogId: item.saleLogId,
+        productId: item.productId,
+        price: item.price,
+        quantity: item.quantity,
+        createdAt: item.createdAt,
+        updatedAt: item.updatedAt,
+        productName: item.product.name,
+        productDescription: item.product.description,
+        total: item.price * item.quantity,
+      })),
     };
   }
 }
